a48c0fc62443526504d89d990bcd725d65213ed6
Ake Hedman <akhe@grodansparadis.com>
2025-11-07 13:27:33 +0100

    Fixed build scripts

--- build-unix.sh
+++ build-unix.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Linux/macOS build script for vscpl2drv-websocksrv
+# Linux/macOS build script for vscpl2drv-logger
 
 mkdir -p build
 cd build
 
-# Configure with Unix Makefiles
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+# Configure with Unix Makefiles generator explicitly
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
 
 # Build the project
 make
